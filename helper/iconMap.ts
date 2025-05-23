45d2c14d92cefae37973aea1d9e12a37ed7b8a54
Alisha <suzycodes@gmail.com>
2025-05-23 13:00:41 -0400

    styled homepage

--- helper/iconMap.ts
+++ helper/iconMap.ts
@@ -1,12 +1,19 @@
 import Logo from "../assets/Logo.svg";
+import Mochi from "../assets/Mochi.svg";
 
 const LogoPng = require("../assets/Logo.png");
+const MochiPng = require("../assets/Mochi.png");
 
 export const ICONS = {
   logo: {
     native: Logo,
     web: LogoPng,
   },
+  mochi: {
+    native: Mochi,
+    web: MochiPng,
+  },
+
   // Add more here as needed
   // myIcon: {
   //   native: require('../assets/MyIcon.svg').default,
